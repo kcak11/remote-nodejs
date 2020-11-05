faebbbdcc75c29ecea6a922603ad2d495edaaaf6
K.C.Ashish Kumar <kcak11@gmail.com>
2020-11-05 12:53:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ app.use(compression());
 
 const httpsAgent = new https.Agent({ rejectUnauthorized: false });
 const basePath = "/adminconsole";
-const staticPath = path.join(__dirname, "/adminconsole");
+const staticPath = path.join(__dirname, "server/adminconsole");
 
 app.get(`${basePath}/health/liveness`, (req, res) => {
   res.sendStatus(200);
